8b3c735df028708cb1e9332c5da36ba3752662da
Developer_o7 <muzaffarmasordinov@gmail.com>
2022-08-18 17:47:31 +0500

    upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let son1
+son1 = prompt('Birinchi sonni kiriting?');
+
+son1 = Number(son1);
+
+let son2
+son2 = prompt('Ikkinchi sonni kiriting?');
+
+son2 = Number(son2);
+
+
+
+let plus = son1 + son2;
+let minus = son1 - son2;
+let kopaytirish = son1 * son2;
+let bolish = son1 / son2;
+let qoldiqliBolish = son1 % son2;
+
+
+
+
+alert('\nQo\shilganda: ' + plus + '\nAyirilganda: ' + minus + '\nKo\paytirilganda: ' + kopaytirish + '\nBo\linganda: ' + bolish + '\nQoldiqli Bo\linganda: ' + qoldiqliBolish + '');
